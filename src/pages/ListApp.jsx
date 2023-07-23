297e00006bdfe0f0f77f1741aa9d9dd8255836cd
Oliver <opaucarq@uni.pe>
2023-07-22 22:03:20 -0500

    update: useState in array

--- src/pages/ListApp.jsx
+++ src/pages/ListApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TableRowContent = ({n, title, state}) => {
   return(
@@ -23,6 +23,7 @@ const Table01 = () => {
             <th>State</th>
           </tr>
         </thead>
+        <tbody>
           <TableRowContent n='1' title="Instalaciones necesarias" state={true}/>
           <TableRowContent n='2' title="Uso de Vite" state={true}/>
           <TableRowContent n='3' title="Componentes" state={false}/>
@@ -32,16 +33,54 @@ const Table01 = () => {
           <TableRowContent n='7' title="useState" state={false}/>
           <TableRowContent n='8' title="Redux" state={true}/>
           <TableRowContent n='9' title="customHooks" state={true}/>  
-        <tbody></tbody>
+        </tbody>
       </table>
     </>
   )
 }
 
+const Table02 = () => {
+  const data = [
+    { title:"Instalaciones necesarias", state:true},
+    { title:"Uso de Vite", state:true},
+    { title:"Componentes", state:false},
+    { title:"Variables en JSX", state:true},
+    { title:"Props", state:true},
+    { title:"Eventos", state:false},
+    { title:"useState", state:false},
+    { title:"Redux", state:true},
+    { title:"customHooks", state:true}
+  ]
+
+  const [array, setArray] = useState(data);
+
+  const handleClick = (e) =>{
+    setArray([...array, {title: "New Element", state: false}])
+  }
+  return(
+    <>
+      <table border={2} bordercolor="red" >
+        <thead>
+          <tr>
+            <th>N°</th>
+            <th>Title</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          {array.map((item,index) => <TableRowContent key={index} n={index+1} title={item.title} state={item.state}/>  )}
+        </tbody>
+      </table>
+      <button onClick={(e)=>handleClick(e)}>Add Element</button>
+    </>
+  )
+}
+
+
 const ListApp = () => {
   return (
     <>
-      <Table01></Table01>
+      <Table02></Table02>
     </>
   )
 }
